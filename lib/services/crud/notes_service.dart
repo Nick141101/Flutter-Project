4d868bc248bfe215d1bc35a206136d2d9801f53c
asus <sokolov141101@gmail.com>
2022-10-23 21:35:29 +0500

    Differentiation of users

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutterproject/extensions/list/filter.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -12,7 +13,10 @@ class NotesService {
 
   List<DatabaseNote> _notes = [];
 
+  DatabaseUser? _user;
+
   static final NotesService _shared = NotesService._sharedInstance();
+
   NotesService._sharedInstance() {
     _notesStreamController = StreamController<List<DatabaseNote>>.broadcast(
       onListen: () {
@@ -20,11 +24,19 @@ class NotesService {
       },
     );
   }
+
   factory NotesService() => _shared;
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream.filter((note) {
+    final currentUser = _user;
+    if (currentUser != null) {
+      return note.userId == currentUser.id;
+    } else {
+      throw UserShouldBeSetBeforeReadingAllNotes();
+    }
+  });
 
   Future<void> _cacheNotes() async {
     final allNotes = await getAllNotes();
@@ -32,12 +44,21 @@ class NotesService {
     _notesStreamController.add(_notes);
   }
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) {
+        _user = user;
+      }
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
@@ -158,7 +179,7 @@ class NotesService {
       userId: owner.id,
       text: text,
     );
-    
+
     _notes.add(note);
     _notesStreamController.add(_notes);
 
@@ -226,9 +247,12 @@ class NotesService {
 
     await getNote(id: note.id);
 
-    final updatesCount = await db.update(noteTable, {
-      textColumn: text,
-    });
+    final updatesCount = await db.update(
+      noteTable,
+      {textColumn: text},
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
 
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
@@ -240,8 +264,6 @@ class NotesService {
       return updatedNote;
     }
   }
-
-
 }
 
 @immutable
